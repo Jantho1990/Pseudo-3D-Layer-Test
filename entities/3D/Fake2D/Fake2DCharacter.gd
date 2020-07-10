f3634ce7722fe99d8fb1ae36a5c3b923763a7826
Josh Anthony <jantho1990@gmail.com>
2020-07-10 15:47:32 -0500

    added layer shifting logic

--- entities/3D/Fake2D/Fake2DCharacter.gd
+++ entities/3D/Fake2D/Fake2DCharacter.gd
@@ -4,18 +4,27 @@ extends KinematicBody
 
 
 # Declare member variables here. Examples:
+export(bool) var free_move = true
 var velocity = Vector3()
 var speed = 4
 var gravity = Vector3.DOWN * 2
 var moving = false
 var direction = Vector2(0, 0)
+var layer_shift = 1
+var is_shifting_layer = false
+var layer_shift_delayed = false
+var layer_shift_delay_time = 1
+var layer_shift_delay_transition_time = 0.3
 
 onready var Animator = $AnimationPlayer
+onready var LayerShiftDelay = $LayerShiftDelay
+onready var LayerShiftTween = $LayerShiftTween
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  LayerShiftDelay.connect('timeout', self, '_on_Layer_shift_delay_stop')
+  LayerShiftTween.connect('tween_all_completed', self, '_on_Layer_shift_tween_all_completed')
 
 
 func _physics_process(delta):
@@ -25,11 +34,30 @@ func _physics_process(delta):
   if not moving: Animator.stop()
   velocity = move_and_slide(velocity, Vector3.UP)
 
+func _on_Layer_shift_delay_stop():
+  layer_shift_delayed = false
+
+func _on_Layer_shift_tween_all_completed():
+  is_shifting_layer = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #  pass
 
 func get_input():
+  if free_move:
+    free_move()
+  else:
+    limited_move()
+
+func animate():
+  match direction:
+    Vector2(-1, 0): Animator.play('walk_w')
+    Vector2(1, 0): Animator.play('walk_e')
+    Vector2(0, -1): Animator.play('walk_n')
+    Vector2(0, 1): Animator.play('walk_s')
+
+func free_move():
   var vy = velocity.y
   velocity = Vector3()
   moving = false
@@ -53,9 +81,39 @@ func get_input():
     
   velocity.y = vy
 
-func animate():
-  match direction:
-    Vector2(-1, 0): Animator.play('walk_w')
-    Vector2(1, 0): Animator.play('walk_e')
-    Vector2(0, -1): Animator.play('walk_n')
-    Vector2(0, 1): Animator.play('walk_s')
+func limited_move():
+  var vy = velocity.y
+  velocity = Vector3()
+  moving = false
+  
+  # Cancel movement if we're shifting layers.
+  if is_shifting_layer: return
+
+  if Input.is_action_pressed('ui_left'):
+    moving = true
+    velocity += -transform.basis.x * speed
+    direction = Vector2(-1, 0)
+  elif Input.is_action_pressed('ui_right'):
+    moving = true
+    velocity += transform.basis.x * speed
+    direction = Vector2(1, 0)
+
+  if not layer_shift_delayed:
+    if Input.is_action_pressed('ui_up'):
+      start_layer_shift_tween(-layer_shift)
+    elif Input.is_action_pressed('ui_down'):
+      start_layer_shift_tween(layer_shift)
+    
+  velocity.y = vy
+
+func start_layer_shift_tween(amount):
+  is_shifting_layer = true
+  var target_translation = translation
+  target_translation.z += amount
+  LayerShiftTween.interpolate_property(self, 'translation', translation, target_translation, layer_shift_delay_transition_time)
+  LayerShiftTween.start()
+  start_layer_shift_delay()
+
+func start_layer_shift_delay():
+  layer_shift_delayed = true
+  LayerShiftDelay.start(layer_shift_delay_time)
