452b0b762cab960153fe5fdab4b69d312d0cdd64
Josh Anthony <jantho1990@gmail.com>
2020-08-22 16:18:06 -0500

    added debugging labels

--- _tests/SpeechBubbleScale/SpeechBubbleScaleTest.gd
+++ _tests/SpeechBubbleScale/SpeechBubbleScaleTest.gd
@@ -3,6 +3,8 @@ extends Spatial
 
 onready var SpeechBubble = $SpeechBubble
 onready var TE = $Control/TextEdit
+onready var L1 = $Control/HBoxContainer/L1
+onready var L2 = $Control/HBoxContainer/L2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +14,5 @@ func _ready():
 
 func _on_text_changed():
   SpeechBubble.raw_text = TE.text
+  L1.text = 'Content Size: ' + String(SpeechBubble.TextboxContainer.content_size)
+  L2.text = 'Total Lines: ' + String(SpeechBubble.TextboxContainer.content_total_lines)
