444f648a9f897748efaa5174ccf534942eb04d0b
Josh Anthony <jantho1990@gmail.com>
2020-08-22 14:22:55 -0500

    added another test for speech bubble scaling

--- systems/dialogue_handler/renderers/DialogueBubbleRenderer/TextboxContainer.gd
+++ systems/dialogue_handler/renderers/DialogueBubbleRenderer/TextboxContainer.gd
@@ -1,22 +1,158 @@
 extends MarginContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const DELIMITERS = [' ', '\n']
+const LINE_BREAK = '\n'
+
+export(int) var max_lines = 5
+export(int) var max_content_width = 360
+
+var total_lines = 0
+var longest_line_width = 0
+
+onready var TextContent = $RichTextLabel
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  print('Lines: ', $RichTextLabel.get_line_count())
-  print('Visible Lines: ', $RichTextLabel.get_visible_line_count())
-  print('Content height: ', $RichTextLabel.get_content_height())
-  # $RichTextLabel.bbcode_text = 'Oh phooey'
-  # print('Lines: ', $RichTextLabel.get_line_count())  
-  # print('Visible Lines: ', $RichTextLabel.get_visible_line_count())
-  # print('Content height: ', $RichTextLabel.get_content_height())
+  total_lines = calculate_total_lines()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+func calculate_total_lines():
+  pass
+
+
+func calculate_longest_line_width():
+  pass
+
+
+###
+# The following are inspired by and converted from the C++ gist posted by Pilvinen
+# https://gist.github.com/Pilvinen/c3a858e2fabe87f3ead967dd15a25d98
+###
+
+func get_pixel_height_for_text(text : String, font : DynamicFont, container_width : int):
+  var font_height = font.get_height()
+
+  var split_text = split_and_keep_delimiters(text, DELIMITERS)
+
+  var line_count = calculate_lines(split_text, font, container_width)
+
+  var pixel_height = line_count * font_height
+
+  pixel_height += font.outline_size * line_count
+
+  return pixel_height
+
+
+func split_and_keep_delimiters(text : String, delimiters : Array):
+  var parts = text.split('')
+  
+  if text.length() > 0:
+    var finished = false
+    var i = 0
+    while not finished:
+      for delimiter in delimiters:
+        var index = parts[i].find(delimiter)
+        if index > -1 and parts[i].length() > index + 1:
+          var left_part = parts[i].substr(0, index + delimiter.length())
+          var right_part = parts[i].substr(index + delimiter.length())
+          parts[i] = left_part
+          parts.insert(i + 1, right_part)
+      if i == parts.size() - 1:
+        finished = true
+      else:
+        i += 1
+  
+  return parts
+
+
+func calculate_lines(split_text : Array, font : DynamicFont, container_width: int):
+  # print('WIDTH: ', container_width)
+  # How many pixels until we need to wrap to next line
+  var width_until_next_line = container_width
+
+  # Keep track of last index
+  var last_index = split_text.size() - 1
+
+  # Keep track of current index
+  var index_ct = 0
+
+  # Keep track of total lines, starting with an assumed first line
+  var total_lines = 1
+
+  # Iterate through the split text and find out how many lines it will be split into
+  for word in split_text:
+    var pixels_in_word = get_word_pixel_width(word, font)
+    # print('Line count: ', total_lines, '. Next line:', width_until_next_line, ', Word: ', word, ' -- ', pixels_in_word, ' pixels wide')
+
+    # If amount of pixels is more than one line can handle, go to next line.
+    if pixels_in_word > width_until_next_line or word.find('\n') > -1:
+      total_lines += 1
+
+      # Subtract word pixel length from next line.
+      width_until_next_line = container_width - pixels_in_word
+    elif pixels_in_word == width_until_next_line:
+      # Increment line counter if we aren't on the last line
+      if index_ct != last_index:
+        total_lines += 1
+      width_until_next_line -= pixels_in_word
+    else:
+      # Subtract word pixel length from next line.
+      width_until_next_line -= pixels_in_word
+    
+    index_ct += 1
+  
+  return total_lines
+
+
+func get_word_pixel_width(word : String, font : DynamicFont) -> int:
+  return int(font.get_string_size(word).x)
+
+
+func format_text_with_line_breaks(text : String, font: DynamicFont, container_width : int):
+  # TODO: Make delimiters a global constant
+  var split_text = split_and_keep_delimiters(text, DELIMITERS)
+
+  # The final returned text
+  var formatted_text = ''
+
+  # How many pixels until we need to wrap to next line
+  var width_until_next_line = container_width
+
+  # Iterate through the split text and find out how many lines it will be split into
+  for word in split_text:
+    var pixels_in_word = get_word_pixel_width(word, font)
+
+    # If amount of pixels is more than one line can handle, go to next line.
+    if pixels_in_word > width_until_next_line:
+      # Inject a line break, because the calculations are not quite accurate enough.
+      # We just need to guarantee that the textbox shows a new line if it calculates it should.
+      word = LINE_BREAK + word
+
+      # Subtract word pixel length from next line.
+      width_until_next_line = container_width - pixels_in_word
+    elif word.find(LINE_BREAK) > -1:
+      width_until_next_line = container_width - pixels_in_word
+    else:
+      width_until_next_line -= pixels_in_word
+    
+    formatted_text += word
+  
+  return formatted_text
+
+
+func get_lines_array(text : String, font: DynamicFont, container_width : int):
+  var formatted_text = format_text_with_line_breaks(text, font, container_width)
+  return formatted_text.split(LINE_BREAK)
+
+
+func get_line_pixel_width(line : String, font: DynamicFont):
+  var split_text = split_and_keep_delimiters(line, DELIMITERS)
+  
+  var total_pixel_width = 0
+  
+  for word in split_text:
+    total_pixel_width += get_word_pixel_width(word, font)
+  
+  return total_pixel_width
