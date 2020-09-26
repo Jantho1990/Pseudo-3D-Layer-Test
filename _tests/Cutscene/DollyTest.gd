23fbd30ee1746520f7ef444bc5c424574eccf4d9
Josh Anthony <jantho1990@gmail.com>
2020-09-26 13:36:06 -0500

    added more dolly functions: dolly direction, dolly reset, signal for other nodes to listen to dolly movement

--- _tests/Cutscene/DollyTest.gd
+++ _tests/Cutscene/DollyTest.gd
@@ -8,9 +8,21 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  $DollyTrack3D.dolly_direction = DollyTrack3D.dolly_directions.DOLLY_REVERSE
   $DollyTrack3D.dolly_active = true
+  $DollyTrack3D.connect('dolly_movement', self, '_on_Dolly_movement')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #  pass
+
+
+func _physics_process(delta):
+  pass
+
+
+func _on_Dolly_movement(data):
+  var dolly_advancement = data.relative_position
+  if dolly_advancement < 0.5:
+    $DollyTrack3D.dolly_direction = DollyTrack3D.dolly_directions.DOLLY_FORWARD
--- systems/cutscene/DollyTrack3D.gd
+++ systems/cutscene/DollyTrack3D.gd
@@ -2,7 +2,18 @@ extends Path
 
 class_name DollyTrack3D # This is used to provide a path for DollyCameras to follow. You can create a path similarly to how a normal Path node works.
 
-export(bool) var dolly_active = false # This controls whether the dolly should be moving.
+signal dolly_movement
+
+const DOLLY_FORWARD = 0
+const DOLLY_REVERSE = 1
+
+enum dolly_directions {
+  DOLLY_FORWARD,
+  DOLLY_REVERSE
+}
+
+export(bool) var dolly_active = false setget set_dolly_active # This controls whether the dolly should be moving.
+export(dolly_directions) var dolly_direction = DOLLY_FORWARD setget set_dolly_direction # Whether the dolly should move backwards or forwards.
 export(bool) var dolly_repeat = false # If set to true, then the dolly movement will restart from zero after reaching the end.
 export(float) var dolly_speed = 1.0 # How fast the dolly moves along the track.
 
@@ -19,6 +30,9 @@ func _ready():
       remove_child(child)
       path_follow_node.add_child(child)
 
+  # Set the dolly up for use.
+  reset_dolly()
+
 
 func _physics_process(delta):
   if dolly_active:
@@ -27,6 +41,53 @@ func _physics_process(delta):
 
 
 func advance_dolly_along_track(speed):
-  path_follow_node.offset += speed
-  if path_follow_node.offset == curve.get_baked_length() and not dolly_repeat:
+  var dolly_sign
+  match dolly_direction:
+    DOLLY_FORWARD: dolly_sign = 1
+    DOLLY_REVERSE: dolly_sign = -1
+  
+  path_follow_node.offset += speed * dolly_sign
+  GlobalSignal.dispatch('debug_label1', { 'text': path_follow_node.offset })
+  GlobalSignal.dispatch('debug_label2', { 'text': speed * dolly_sign })
+  
+  if dolly_reached_end_of_track() and not dolly_repeat:
     dolly_active = false
+
+  emit_signal('dolly_movement', {
+    'track_position': path_follow_node.offset, # The position of the dolly along the track.
+    'dolly_direction': dolly_direction, # Which direction the dolly is moving.
+    'relative_position': path_follow_node.unit_offset # Relative position along the track, from 0-1
+  })
+
+
+func dolly_reached_end_of_track():
+  match dolly_direction:
+    DOLLY_FORWARD: return path_follow_node.offset == curve.get_baked_length()
+    DOLLY_REVERSE: return path_follow_node.offset == 0
+
+
+func get_track_length():
+  return curve.get_baked_length()
+
+
+func reset_dolly():
+  print('reset')
+  match dolly_direction:
+    DOLLY_FORWARD:
+      path_follow_node.offset = 0
+      print('forward')
+    DOLLY_REVERSE: path_follow_node.offset = curve.get_baked_length()
+
+
+func set_dolly_active(value : bool):
+  dolly_active = value
+  if dolly_reached_end_of_track():
+    reset_dolly()
+
+
+func set_dolly_direction(value):
+  match value:
+    DOLLY_FORWARD, DOLLY_REVERSE: dolly_direction = value
+  
+  if dolly_reached_end_of_track():
+    reset_dolly()
